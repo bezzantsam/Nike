af5a3063cc64cb4db91edf9ec433450d0dc9cc10
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-21 00:42:56 -0700

    components rendering to App

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts' 
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'   
+import CustomerReviews from './CustomerReviews'   
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+export{
+    Subscribe, Footer,Hero,PopularProducts,SuperQuality,Services,SpecialOffer, CustomerReviews
+}
