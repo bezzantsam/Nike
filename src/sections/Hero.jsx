4afd35cc8c9d424a6f68f097a06fc5bb8925b0a7
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-22 00:29:16 -0700

    Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,19 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section 
+    id='home'
+    className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+      <p> Our summer collection</p>
+        <h1>
+          <span>The new arrival</span>
+          <br />
+          <span> Nike </span>
+          Shoes
+          </h1>
+      </div>
+    </section>
   )
 }
 
