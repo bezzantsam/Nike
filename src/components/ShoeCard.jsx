58358a6e055b270cdd5f621275b32db3ec31d3b6
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-23 14:45:57 -0700

    popular product card

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,16 +1,18 @@
-const ShoeCard = ({imgURL, changeBigShoe, bigShoeImg}) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     const handleClick = () => {
-        if(bigShoeImgh !== imgURL.bigShoe){
-            changeBigShoeImage(imgURL.bigShoe)
-        }
-    }
+      if (bigShoeImg !== imgURL.bigShoe) {
+        changeBigShoeImage(imgURL.bigShoe);
+      }
+    };
   return (
-    <div className={`border-2 rounded-xl
-    ${bigShoeImg === imgURL.bigShoe}
-    ? 'border-coral-red' : 'border-transparent'}
-    cursor-pointer max-sm:flex1`} 
+    <div
+    className={`border-2 rounded-xl ${
+      bigShoeImg === imgURL.bigShoe
+        ? "border-coral-red"
+        : "border-transparent"
+    } cursor-pointer max-sm:flex-1`}
     onClick={handleClick}
-    >
+  >
     <div className="flex justify-containe-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p4">
         <img 
         src={imgURL.thumbnail}
