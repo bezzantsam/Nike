a3ec7d95f2906bf4f63347241d47b463ec67b4d6
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-23 08:33:22 -0700

    shpecard bg

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,26 @@
-const ShoeCard = () => {
+const ShoeCard = ({imgURL, changeBigShoe, bigShoeImg}) => {
+    const handleClick = () => {
+        if(bigShoeImgh !== imgURL.bigShoe){
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    }
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2 rounded-xl
+    ${bigShoeImg === imgURL.bigShoe}
+    ? 'border-coral-red' : 'border-transparent'}
+    cursor-pointer max-sm:flex1`} 
+    onClick={handleClick}
+    >
+    <div className="flex justify-containe-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p4">
+        <img 
+        src={imgURL.thumbnail}
+        alt="shoe collection"
+        width={127}
+        height={103}
+        className="object-contain"
+        />
+    </div>
+    </div>
   )
 }
 
