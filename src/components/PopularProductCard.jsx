9b322271e44cff3173c154680515d183f0c6efda
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-23 22:25:36 -0700

    popular products card images

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,6 +1,13 @@
-const PopularProductCard = () => {
+const PopularProductCard = ({imgURL, name, price}) => {
   return (
-    <div>PopularProductCard</div>
+  <div className="flex flex-1 flex-col w-full max-sm:w-full">
+  <img
+    src={imgURL} alt={name} className='w-[280px] h-[280px]'
+  />
+ 
+  
+
+  </div>
   )
 }
 
