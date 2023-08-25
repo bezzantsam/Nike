33897cce0fda6efc9febec3b3c7a4c14dda6f24a
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-24 19:46:19 -0700

    special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
   return (
-   <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
+   <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+   ${
+     backgroundColor
+       ? `${backgroundColor} ${textColor} ${borderColor}`
+       : "bg-coral-red text-white border-coral-red"
+   } rounded-full {fullWidth && "w-full"}`}
+ >
     {label}
     {iconURL && (
         <img
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,12 +1,34 @@
+import {arrowRight} from '../assets/icons' 
 import {offer} from '../assets/images'
+import Button from '../components/Button'
 const SpecialOffer = () => {
   return (
     <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
       <div className="flex-1">
         <img  src={offer} width={773} height={687} className="object-contain w-full"/>
       </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+          <span className='text-coral-red'> Special </span> Offer
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+        Step into a realm of unparalleled innovation and timeless style with our Exclusive Offer collection. This handpicked selection embodies a blend of iconic design and modern performance, representing both our legacy and our future aspirations. From revolutionary athletic designs to classic silhouettes, this collection has it all.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          But its not just about aesthetic appeal. Each shoe is engineered with cutting-edge technology to provide an edge, whether on the track, the court, or the streets. Dive into this curated range and discover unmatched excellence at a special rate, available for a limited time. With each step, experience the essence of true greatness.
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
