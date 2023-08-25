e790de45cb2c20464558628cc8bfd32c8043b339
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-24 18:10:47 -0700

    special offer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,11 @@
+import {offer} from '../assets/images'
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img  src={offer} width={773} height={687} className="object-contain w-full"/>
+      </div>
+    </section>
   )
 }
 
