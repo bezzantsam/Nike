1ab4ef0c9dc8046cba392ed6f98ac52999540c9c
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-24 21:40:01 -0700

    customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,27 @@
+import ReviewCard from '../components/ReviewCard'
+import {reviews} from '../constants/index.js';
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
-  )
-}
+    <section className="max-container">
+      <h3 className='font-palanquin text-center text-4xl font-bold'>What Our
+      <span className='text-coral-red'> Customers </span>
+      Say?
+      </h3>
+      <p className="info-text m-auto max-w-lg text-center">Hear genuine stories from our satisfied customers</p>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customername}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CustomerReviews
+export default CustomerReviews;
