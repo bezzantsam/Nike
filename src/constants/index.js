0d7d0de21fe51b464e801799c7fd223b201898df
Sam-Bezzant <bezzantsam@gmail.com>
2023-08-26 12:20:34 -0700

    sam added

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,8 +73,8 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
+        customerName: 'Sam Bezzant',
+        rating: 5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
     {
